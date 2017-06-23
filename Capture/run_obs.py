29401d2335a3513d84c74fed6308dab41884f08b
Greg Dobler <greg.dobler@gmail.com>
2017-06-23 14:31:25 -0400

    added scheduling

--- Capture/run_obs.py
+++ Capture/run_obs.py
@@ -29,6 +29,9 @@ def do_capture(ec):
     # -- capture stack
     ec.capture_stack(nimg, base.format(cnt[0]), hgt, wid)
 
+    # -- update counter
+    cnt[0] += 1
+    
     return
 
 
@@ -55,6 +58,7 @@ def pan_to_position(pan, tilt):
 
 
 # -- utilities
+cnt    = [0]
 pt     = (95, -255)
 nimg   = 20
 tint   = 30 # 33188 # ms
@@ -64,19 +68,22 @@ params = [nimg, "stack_bb_{0:07}.raw", 0, 0, tint]
 # -- set pan/tilt params and move to position
 pan_to_position(pt[0], pt[1])
 
+
 # -- set up the image capture
 ec   = ECapture()
 ec.get_camera(ec.detect_camera())
 
+
 # -- read the configuration files
 ec._readConfigFiles()
 
-# -- get parameters necessary to start camera stream
-wid = ec.read_parameter('Width')[0]
-hgt = ec.read_parameter('Height')[0]
 
+# -- get parameters necessary to start camera stream
+wid       = ec.read_parameter('Width')[0]
+hgt       = ec.read_parameter('Height')[0]
 params[3] = wid
 params[2] = hgt
+
 
 # -- open the camera stream, disbale HDR and white balance, and set exposure
 ec.open_cam_stream()
@@ -84,33 +91,26 @@ ec.set_parameter('HDREnable', False)
 ec.set_parameter('WB_Enable', False)
 ec.set_parameter('Exposure', params[4])
 
+
 # -- set up the scheduler
-# times = range(21 * 3600, (21 + 9) * 3600, 10)
-
-# t0 = times[1000]
-
-# hr = t0 // 3600
-# mn = t0 - hr * 3600
+sec   = np.arange(21 * 3600, (21 + 9) * 3600, 10)
+times = ["{0:02}:{1:02}:{2:02}".format(i, j, k) for i, j, k in
+         zip(sec // 3600 % 24, sec % 3600 // 60, sec % 60)]
+dum   = [schedule.every().day.at(i).do(do_capture) for i in times]
 
 
-
-# ["21:00:00", 
-# "21:00:00", 
-# "21:00:00", 
-# "21:00:00", 
-# "21:00:00", 
-
-schedule.every(
-
+# -- start image acquisition
 try:
-    # -- get a stack
-    ec.capture_stack(10, "temp.raw", hgt, wid)
+    # -- run scheduler
+    while True:
+        schedule.run_pending()
+        time.sleep(0.1)
 
     # -- release camera
     ec.close_cam_stream()
     ec.release_camera()
 except Exception as exc:
-    print("CAPTURE FAILED!!!")
+    print("CAPTURE {0} FAILED!!!".format(cnt[0]))
     print(exc)
 
     # -- release camera
